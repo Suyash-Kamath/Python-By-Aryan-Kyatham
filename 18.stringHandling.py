de07e55000e0161e9f538ebdd5c5835cf56e6960
Suyash Kamath <suyashkamath9@gmail.com>
2025-06-12 13:32:51 +0530

    Updated File number 18

--- 18.stringHandling.py
+++ 18.stringHandling.py
@@ -1,2 +1,23 @@
 # Strings are sequences of textual data enclosed within single or double quotation marks.
 
+# Accessing Character
+
+
+name = "Suyash"
+
+print(name[0])
+print(name[1])
+
+# Strings can be accessed like arrays with indices starting from 0.
+
+'''
+String Slicing
+Slicing refers to extracting parts of a string using indices.
+
+'''
+
+fruit="Mango"
+
+len1=len(fruit)
+
+print("Mango is a ",len1,"letter word")
