9da2126a93825dfcf61ee3f2b57c6a799e8a2a59
Suyash Kamath <suyashkamath9@gmail.com>
2025-06-12 15:33:28 +0530

    New File Number 20 added

---
+++ 20-conditionalOperators.py
@@ -0,0 +1,8 @@
+# Conditional operators are used to compare values. Here are some examples:
+
+
+a = 18
+print(a > 18)   # Output: False
+print(a <= 18)  # Output: True
+print(a == 18)  # Output: True
+print(a != 18)  # Output: False
